f26a9e6239461f9e3f1ecca9764c60fc0d629e93
Serhii Melnychenko <higankingpro@gmail.com>
2022-02-12 22:15:41 +0200

    tasks 1.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const divEl = document.querySelector('.js-gallery');
+
+
+
+
+createGallery(galleryItems);
+function createGallery(items) {
+    const markup = items.map((item) =>
+      
+        `<div class="gallery__item">
+ <a class="gallery__link"
+      href="${item.original}"
+       >
+         <img
+           class="gallery__image"
+           src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+       />
+      </a>
+</div>`
+  ).join('');
+    divEl.innerHTML = markup
+};
+
